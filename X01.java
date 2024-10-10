7ca8efcbad759980d63a4d7622cbfde78ccb7c32
RotuaTampubolon <rotuaimmanuela@gmail.com>
2024-10-09 15:36:18 -1200

    Telah selesai<3
    Pair-01

--- X01.java
+++ X01.java
@@ -1,2 +1,33 @@
-// NIM - Your Name
-// NIM - Your Name
+// 12S24028 - Rotua Immanuela Tampubolon
+// 12S24002 - Petra Ignatius Pengayoman Naibaho
+import java.util.*;
+import java.lang.Math;
+
+public class X01 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        double price, small;
+        double total, total1;
+
+        small = 999.99;
+        total = 0;
+        price = input.nextDouble();
+        while (price != 0) {
+            if (price < small) {
+                small = price;
+                total = total + small;
+            } else {
+                price = price;
+                total = total + price;
+            }
+            price = input.nextDouble();
+        }
+        if (total >= 100.0) {
+            total1 = total - small;
+        } else {
+            total1 = total;
+        }
+        System.out.println(Toxifed(total1, 2));
+    }
+}
